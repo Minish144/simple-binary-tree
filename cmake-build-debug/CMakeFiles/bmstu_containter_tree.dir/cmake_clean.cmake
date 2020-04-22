c94bbd6e32e253182e350c24bc836a3b11506809
Nikolai Varlamov <varlamow.col@yahoo.com>
2020-04-22 14:43:06 +0300

    git init

---
+++ cmake-build-debug/CMakeFiles/bmstu_containter_tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmstu_containter_tree.dir/main.cpp.o"
+  "bmstu_containter_tree"
+  "bmstu_containter_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bmstu_containter_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
